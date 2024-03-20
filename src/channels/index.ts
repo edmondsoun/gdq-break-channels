abedf377515ff4bfa0ca7daf0ce75d157282275b
Edmond Soun <edmond.soun@gmail.com>
2024-03-20 13:03:33 -0700

    update channel src and package lock for demo deploy

--- src/channels/index.ts
+++ src/channels/index.ts
@@ -1,21 +1,21 @@
 import './channels';
 
-import './breakout';
-import './cave-story';
-import './desert-bus';
-import './mega-man';
-import './mgs3';
-import './monkey-island';
-import './oregon-trail';
-import './tetris';
-import './spyro';
-import './undertale';
-import './t-rex-runner';
-import './dragon-warrior';
-import './ff-shop';
-import './template';
-import './papers-please';
-import './minesweeper';
 import './galaga';
+// import './breakout';
+// import './cave-story';
+// import './desert-bus';
+// import './mega-man';
+// import './mgs3';
+// import './monkey-island';
+// import './oregon-trail';
+// import './tetris';
+// import './spyro';
+// import './undertale';
+// import './t-rex-runner';
+// import './dragon-warrior';
+// import './ff-shop';
+// import './template';
+// import './papers-please';
+// import './minesweeper';
 
 export * from './channels';
